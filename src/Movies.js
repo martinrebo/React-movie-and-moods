bb5963d84bf16e95b0dd985d71dba9356afd7718
martinrebo <martinrg33@gmail.com>
2019-02-28 07:53:51 +0100

    working movie component

--- src/Movies.js
+++ src/Movies.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import Slider from "react-slick"; 
+import "../node_modules/slick-carousel/slick/slick.css";
+import "../node_modules/slick-carousel/slick/slick-theme.css";
 
 function Movies() {
 
     const URL = 'https://api.themoviedb.org/3/discover/movie?api_key=aedc983b8ecbee54751dd5b6e682190e';
     const URL_IMG = 'https://image.tmdb.org/t/p/w200/';
-
-    const genresList = { "genres": [{ "id": 28, "name": "Action" }, { "id": 12, "name": "Adventure" }, { "id": 16, "name": "Animation" }, { "id": 35, "name": "Comedy" }, { "id": 80, "name": "Crime" }, { "id": 99, "name": "Documentary" }, { "id": 18, "name": "Drama" }, { "id": 10751, "name": "Family" }, { "id": 14, "name": "Fantasy" }, { "id": 36, "name": "History" }, { "id": 27, "name": "Horror" }, { "id": 10402, "name": "Music" }, { "id": 9648, "name": "Mystery" }, { "id": 10749, "name": "Romance" }, { "id": 878, "name": "Science Fiction" }, { "id": 10770, "name": "TV Movie" }, { "id": 53, "name": "Thriller" }, { "id": 10752, "name": "War" }, { "id": 37, "name": "Western" }] }
 
     //Hooks
     const [isLoading, setIsLoading] = useState(true);
@@ -39,32 +40,43 @@ function Movies() {
             });
     }
 
+    // slider 
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1
+      };
+
 
     return (
 
         <div>
+            ----- Buttons ---- 
+            <div> 
             <button onClick={() => setGenre(878)}>Gernes button set</button>
             <button onClick={() => setData(data)}>Data button set</button>
+            </div>
             ------ Genre ID --------
-
             <div id="genre"> </div>
-
             -------------State Data ---------------
-
-
-             <div id="stateData"> </div>
-
             {isLoading ? (
+<div>
                 "loading ...  "
+</div>
+
             ) : (
-                    <div>{data.results.map((obj) =>
+                <Slider {...settings}>
+                    {data.results.map((obj) =>
                         <div key={obj.id}>
                             <div> {obj.vote_count} </div>
                             <img src={URL_IMG + obj.poster_path} alt=""></img>
                             <div>  {obj.title} </div>
                         </div>
                     )}
-                    </div>
+                    
+                    </Slider>
                 )}
         </div>
 
